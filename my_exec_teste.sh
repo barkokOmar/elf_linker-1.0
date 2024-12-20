986dfb8b44daa44d6f0ddc83562119a8d66ac9a7
barkoko <barkokomar@gmail.com>
2024-12-20 12:37:41 +0100

    refactor: factorise read_header (non stable)

--- my_exec_teste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-example=$1
-
-make 
-echo "============================= Resultat Attendu:"
-arm-none-eabi-readelf -h $example 
-echo "============================= Resultat Obtenu:"
-./my_test_affichages $example
